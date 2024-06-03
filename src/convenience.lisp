7221e63482b7e25e9defa893f2162b36b4b1d4c8
David McClain <dbm@refined-audiometrics.com>
2024-06-03 06:53:49 -0700

    move pic-fmt to convenience.lisp

--- src/convenience.lisp
+++ src/convenience.lisp
@@ -50,3 +50,80 @@
       )
      ,@body
      ))
+
+;; -----------------------------------------------
+
+(defun pic-fmt (s)
+  ;; Construct a Lisp format string from a pictured output.
+  ;;
+  ;; S = text place, eg., SSSS => ~4A
+  ;; # = digit place, eg, ###  => ~3D
+  ;; 0 = forced zero-fill digit place, must be first in pic, e.g., 0## => ~3,'0D
+  ;; - = sign place       -    => ~C
+  ;; f = float place, eg., fff.ff => ~6,2F
+  ;;
+  (let* ((fmt  "")
+         (start 0)
+         (dpl   nil)
+         state)
+    (macrolet ((state (new-state)
+                 `(setf state #',new-state)))
+      (labels ((start (ix c)
+                 (setf start ix)
+                 (case (char-downcase c)
+                   (#\s         (state str))
+                   ((#\0 #\#)   (state num))
+                   (#\-         (accum "~c"))
+                   (#\f         (setf dpl nil)
+                                (state flt))
+                   (#\Null )
+                   (t           (state idle))
+                   ))
+               (idle (ix c)
+                 (case (char-downcase c)
+                   ((#\s #\0 #\# #\- #\f)
+                    (accum (subseq s start ix))
+                    (start ix c))
+                   (#\Null
+                    (accum (subseq s start ix)))
+                   ))
+               (str (ix c)
+                 (case (char-downcase c)
+                   (#\s )
+                   (t   (accum (format nil "~~~dA" (- ix start)))
+                        (start ix c))
+                   ))
+               (num (ix c)
+                 (case c
+                   ((#\0 #\#) )
+                   (t
+                    (cond ((char= #\0 (char s start))
+                           (accum (format nil "~~~d,'0D" (- ix start))))
+                          (t
+                           (accum (format nil "~~~dD" (- ix start)))))
+                    (start ix c))
+                   ))
+               (flt (ix c)
+                 (case c
+                   (#\f )
+                   (#\.
+                    (setf dpl ix))
+                   (t
+                    (if dpl
+                        (accum (format nil "~~~d,~dF" (- ix start) (- ix dpl 1)))
+                      (accum (format nil "~~~~dF" (- ix start))))
+                    (start ix c))
+                   ))
+               (accum (s)
+                 (setf fmt (concatenate 'string fmt s))))
+        (setf state #'start)
+        (loop for ix from 0 below (length s) do
+                (funcall state ix (char s ix)))
+        (funcall state (length s) #\Null)
+        fmt
+        ))))
+
+#|
+(pic-fmt "ssss  ssssss  ssssss  ssssssssss  0# 0# 0#.#  -0# 0# 0#    ff.fff  ff.fff   ff.ff~%")
+|#
+
